27cc6a675e84221ce57597f41fcee4b759526ea1
crash <contact@gamedetectives.net>
2021-02-25 13:24:47 -0700

    updated offsets for 1.20.3a

--- JesterCap/ProcessReader.cs
+++ JesterCap/ProcessReader.cs
@@ -12,9 +12,9 @@ namespace JesterCap
         private const int PROCESS_WM_READ = 0x0010;
 
         // These offsets need to be updated with each new version of Spelunky 2.
-        // Current as of 1.20.2a (2021-02-04)
-        private const int OFFSET_GAME_DATA = 0x221AE448;
-        private const int OFFSET_SCORE = 0x221A93C0;
+        // Current as of 1.20.3a (2021-02-25)
+        private const int OFFSET_GAME_DATA = 0x22306448;
+        private const int OFFSET_SCORE = 0x223014B0;
         private const int OFFSET_FRAME_COUNT = 0xEA0;      // the number of frames spent in the current level (unpaused)
         private const int OFFSET_NUM_ITEMS = 0X77E;        // the number of items picked up
         private const int OFFSET_FIRST_ITEM_ID = 0x784;    // the first item ID in memory

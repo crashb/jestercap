0afc408e434ce7d1a481d7596f6312e2e64ba560
HowardCLH <94495870+HowardCLH@users.noreply.github.com>
2022-01-19 19:37:12 -0500

    Update ProcessReader.cs

--- JesterCap/ProcessReader.cs
+++ JesterCap/ProcessReader.cs
@@ -12,9 +12,9 @@ namespace JesterCap
         private const int PROCESS_WM_READ = 0x0010;
 
         // These offsets need to be updated with each new version of Spelunky 2.
-        // Current as of 1.25.0b (2021-11-16)
-        private const int OFFSET_GAME_DATA = 0x22DB7F08;
-        private const int OFFSET_SCORE = 0x22DC3884;
+        // Current as of 1.25.2 (2022-01-19)
+        private const int OFFSET_GAME_DATA = 0x22DC7F88;
+        private const int OFFSET_SCORE = 0x22DD3914;
         private const int OFFSET_FRAME_COUNT = 0xEE4;      // the number of frames spent in the current level (unpaused)
         private const int OFFSET_NUM_ITEMS = 0X77E;        // the number of items picked up
         private const int OFFSET_FIRST_ITEM_ID = 0x784;    // the first item ID in memory
